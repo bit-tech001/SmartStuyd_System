012d0fdf0fc1b7afe2eae11aa82fe9899764f14f
@BituPathak <tasus0833@gmail.com>
2026-07-04 17:45:29 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -17,7 +17,7 @@ import cors from "cors";
 
 app.use(
   cors({
-    origin: "https://smart-stuyd-system-gguk.vercel.app",
+    origin: "https://smart-stuyd-system-s98i.vercel.app/",
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
